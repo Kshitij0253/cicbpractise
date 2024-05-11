e1492169b737953535a532a7b0fcfcb7d9781039
Kshitij0253 <ks904125@gmail.com>
2024-05-11 19:43:23 +0530

    ading comment in nodemon ,js

--- NodemMain.js
+++ NodemMain.js
@@ -1,3 +1,4 @@
 console.log("Nodejs file ");
 console.log("testing with node ");
 console.log("add one more line");
+console.log("we learn first build triger opption in gaurav sham playlist thah how we run run build trigger remotely");
